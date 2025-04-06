ed86102a2e48e711ae53d12c7d62ca73116bea52
ashish_1 <messiforeverashish2022@gmail.com>
2025-04-06 18:43:24 +0530

    Added multiplication function

--- calculator.py
+++ calculator.py
@@ -1,6 +1,11 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b  # New multiplication function
+
 print("Welcome to Calculator")
 print("2 + 3 =", add(2, 3))
+print("3 * 4 =", multiply(3, 4))  # Multiplication example
+
 
